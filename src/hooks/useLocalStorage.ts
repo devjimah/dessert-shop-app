7b51d7aa07ab42e397d94ee3332443791c9843e2
devjimah <devjimah@gmail.com>
2026-02-09 09:29:08 +0000

    refactor: remove unnecessary comment in useLocalStorage hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -14,7 +14,6 @@ export default function useLocalStorage<T>(key: string, initialValue: T): [T, (v
   const setValue = useCallback((value: T | ((prev: T) => T)) => {
     try {
       setStoredValue(prevValue => {
-        // Allow value to be a function so we have same API as useState
         const valueToStore = value instanceof Function ? value(prevValue) : value;
         window.localStorage.setItem(key, JSON.stringify(valueToStore));
         return valueToStore;
